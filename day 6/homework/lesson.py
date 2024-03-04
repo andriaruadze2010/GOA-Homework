222879a116d559c67de73714a4d00495ecb10620
andriaruadze2010 <ruadgabrii@gmail.com>
2024-03-04 08:59:56 +0400

    7th commit

---
+++ day 6/homework/lesson.py
@@ -0,0 +1,11 @@
+num1 = input("please enter yout number")
+num2 = input("please enter your number")
+num3 = input("please enter your number")
+
+print(num1 + num2 + num3)
+
+print(num1 - num2 - num3)
+
+print(num1 * num2 * num3)
+
+print(num1 / num2 / num3)
