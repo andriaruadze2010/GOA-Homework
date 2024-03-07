f5252fc79cdaae623ac00e5417d38c7c90f6a96f
andriaruadze2010 <ruadgabrii@gmail.com>
2024-03-07 08:43:22 +0400

    8th commit

---
+++ day 7/homework/lesson7.py
@@ -0,0 +1,25 @@
+print(int(5.2))
+print(int("3") + 5)
+
+
+print(float("3.2"))
+print(float(1))
+
+
+
+print(str(5) + "7")
+print(str(3.4) + "2")
+
+
+
+
+
+
+print(type(1.1))
+print(type(1))
+print(type("andria"))
+
+
+
+
+
