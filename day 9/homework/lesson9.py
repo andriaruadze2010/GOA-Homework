990dabcaab73f0e2094ef92ccc64f1cc851b31f2
andriaruadze2010 <ruadgabrii@gmail.com>
2024-03-18 20:48:21 +0400

    10th commit

---
+++ day 9/homework/lesson9.py
@@ -0,0 +1,22 @@
+print(9 < 5)
+print(9 > 5)
+
+
+print(type(3.9))
+print(type(7))
+print(type(andria))
+
+
+
+
+num1 = input("please enter yout number")
+num2 = input("please enter your number")
+num3 = input("please enter your number")
+
+print(num1 + num2 + num3)
+
+print(num1 - num2 - num3)
+
+print(num1 * num2 * num3)
+
+print(num1 / num2 / num3)
