e8c256da02ef66c08d6357dc4c020c15ee846275
andriaruadze2010 <ruadgabrii@gmail.com>
2024-03-24 16:02:36 +0400

    14  th commit

---
+++ day 13/classwork/lesson13.py
@@ -0,0 +1,6 @@
+number1 = int(input("please enter your number"))
+
+if number1 > 0:
+    print("the number is positive")
+else:
+    print("the number is negative")
---
+++ day 13/homework/lesson13.py
@@ -0,0 +1,13 @@
+for i in range(1 , 51):
+    if i % 3 ==0:
+        print(i)
+
+
+
+
+
+
+
+for i in range(2 , 101 , ):
+    if i % 2 ==0:
+        print(i)
