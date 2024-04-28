eee8da56ddb84a8747285abaef7e9913f091dcac
andriaruadze2010 <ruadgabrii@gmail.com>
2024-04-29 00:07:23 +0400

    25th commit

---
+++ day 24/classwork/lesson24.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 11
+
+def add(number1,number2):
+    print(number1+number2)
+add(number1, number2)
---
+++ day 24/homework/lesson24.py
@@ -0,0 +1,20 @@
+number1 = 20
+number2 = 15
+def add(number1,number2):
+    print(number1+number2)
+add(number1,number2)
+
+
+nick1 = "andria"
+nick2  = "gabrieli"
+def add(nick1 , nick2):
+    print(nick2 + "borther is andria",nick1 + "brother is gabrieli")
+add(nick1,nick2)
+
+
+num1 = 32
+num2 = 4
+
+def multyply(num1,num2):
+    print(num1*num2)
+multyply(num1,num2)
