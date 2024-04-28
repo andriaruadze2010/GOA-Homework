64c2caf25957a71b34bdb45b8ee5d6bee0d2870d
andriaruadze2010 <ruadgabrii@gmail.com>
2024-04-28 23:41:18 +0400

    23th commit

---
+++ day 22/classwork/leeson22.py
@@ -0,0 +1,65 @@
+list = []
+start = 0
+end = 9
+step = 1
+
+while start < end:
+    list.append(start)
+    start = start = step
+print(list)
+
+names = ["Luka", "Lile", "Nia", "Mate", "Ruti"]
+
+sliced_list = []
+
+start = 0
+end = 5
+step = 2
+
+while start < end:
+    sliced_list.append(names[2])
+    start = start + step
+
+print(sliced_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string = "andria"
+print(string[4:6])
+
+
+
+
+numbers_list = []
+start = 5
+end = 5
+step = -1
+
+
+
+while start > end:
+    list.append(start)
+    start = start + step
+print(numbers_list)
+
+
+
+
+
+
+
+
---
+++ day 22/homework/lesson22.py
@@ -0,0 +1,29 @@
+
+#მომხმარებელს შემოატანინეთ ნებისმიერი სიტყვა. თუ მისი სიგრძე აღემატება სამს, დაბეჭდეთ მისი პირველი სამი ასო. ხოლო თუ ნაკლებია ან ტოლი სამის, დაბეჭდეთ სიტყვის პირველი ასო.
+
+
+
+word = (input("please enter your word"))
+
+if len(word) > 3:
+    print(word[:3])
+else:
+    print(word[0])
+
+
+
+
+numbers = (range(10, 21))
+
+for i in range(len(numbers)):
+    if i % 2 == 0:
+        print(numbers[i])
+
+
+
+word = input("შეიყვანეთ სიტყვა: ")
+
+
+word = None
+
+print("შეტანილი სიტყვა:", word)
