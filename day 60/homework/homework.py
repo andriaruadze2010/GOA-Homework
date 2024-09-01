a0896c1707efe740738a1330283262ed62ed26f8
andriaruadze2010 <ruadgabrii@gmail.com>
2024-09-01 16:43:30 +0400

    61th commit

---
+++ day 60/homework/homework.py
@@ -0,0 +1,10 @@
+def solution(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return False
+
+    num = 0
+
+    for i in range(len(arr1)):
+        num += (arr1[i] - arr2[i]) ** 2
+
+    return num / len(arr1)
